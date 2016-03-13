51dca8296819364f53ea7e702c75bd476ba6d320
13Dima13 <sda.freeman@gmail.com>
2016-03-13 23:31:53 +0200

    Availability groups before delete

--- addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupModificationTests.java
@@ -7,7 +7,9 @@ public class GroupModificationTests extends TestBase{
   @Test
   public void testGroupModification () {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().selectGroup();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().selectGroup();
+    }
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test4"));
     app.getGroupHelper().submitGroupModification();
