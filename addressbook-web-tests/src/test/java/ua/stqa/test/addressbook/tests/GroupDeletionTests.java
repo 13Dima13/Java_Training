e7fc0d0bbeb9659b8d0a5abcfaac43ab224b507f
13Dima13 <sda.freeman@gmail.com>
2016-03-14 00:00:06 +0200

    More checks for delete Groups

--- addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupDeletionTests.java
@@ -9,10 +9,14 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()) {
+        if (app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().selectGroup();
+        } else {
+            app.getGroupHelper().initGroupCreation();
+            app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test4"));
             app.getGroupHelper().submitGroupCreation(new GroupData("test1", null, null));
+            app.getGroupHelper().returnToGroupPage();
         }
-        app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
     }
