a2ff91d68a427efcb006604be2bb1fc9f7f317cf
13Dima13 <sda.freeman@gmail.com>
2016-03-20 00:13:51 +0200

    Change massive

--- sandbox/src/main/java/ua/stqa/test/sandbox/sandbox/Collections.java
+++ sandbox/src/main/java/ua/stqa/test/sandbox/sandbox/Collections.java
@@ -3,10 +3,10 @@ package ua.stqa.test.sandbox.sandbox;
 
 public class Collections {
   public static void main (String[] args) {
-    String[] langs = {"Java","C#", "Python","PHP"};
+    String[] langs = {"Java", "C#", "Python", "PHP"};
 
-    for (int i = 0; i< langs.length; i++ ) {
-      System.out.println("I want to learn " + langs[i]);
+    for (String l : langs) {
+      System.out.println("I want to learn " + l);
     }
   }
 }
