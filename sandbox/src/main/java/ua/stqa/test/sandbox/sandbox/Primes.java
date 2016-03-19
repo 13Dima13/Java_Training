aa2b61e13aba7c8291789765297eb3863123bb87
13Dima13 <sda.freeman@gmail.com>
2016-03-19 23:55:09 +0200

    Cycles (l4_1) _ 2

--- sandbox/src/main/java/ua/stqa/test/sandbox/sandbox/Primes.java
+++ sandbox/src/main/java/ua/stqa/test/sandbox/sandbox/Primes.java
@@ -3,11 +3,20 @@ package ua.stqa.test.sandbox.sandbox;
 
 public class Primes {
   public static boolean isPrime(int n) {
-    for (int i = 2; i<n; i ++) { //Цикл (+= вместо i+1б, ++ - инкрементирование)
+    for (int i = 2; i < n; i++) { //Цикл (+= вместо i+1б, ++ - инкрементирование)
       if (n % i == 0) {
         return false;
       }
     }
     return true;
+  }
+
+
+  public static boolean isPrimeWhile(int n) {
+    int i = 2;
+    while (i<n && n % i != 0) { 
+      i++;
+    }
+    return i == n;
   }
 }
