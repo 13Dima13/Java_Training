47e39386f7d169e9551bc04ed1d2803651ae8157
13Dima13 <sda.freeman@gmail.com>
2016-03-20 00:11:28 +0200

    Коллекции / Массивы

---
+++ sandbox/src/main/java/ua/stqa/test/sandbox/sandbox/Collections.java
@@ -0,0 +1,12 @@
+package ua.stqa.test.sandbox.sandbox;
+
+
+public class Collections {
+  public static void main (String[] args) {
+    String[] langs = {"Java","C#", "Python","PHP"};
+
+    for (int i = 0; i< langs.length; i++ ) {
+      System.out.println("I want to learn " + langs[i]);
+    }
+  }
+}
