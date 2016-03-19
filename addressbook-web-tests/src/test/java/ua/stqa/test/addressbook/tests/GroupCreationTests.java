96af0b5d4f7be5aaa00e59bff9eb7451d0d784df
13Dima13 <sda.freeman@gmail.com>
2016-03-20 00:45:04 +0200

    Added getGroupCount

--- addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ua.stqa.test.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.stqa.test.addressbook.model.GroupData;
 
@@ -8,10 +9,14 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test4"));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before +1);
+
   }
 
 }
