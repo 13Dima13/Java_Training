433e279e4296ac042be9e2f6f3c740d38b37f368
13Dima13 <sda.freeman@gmail.com>
2016-03-19 23:59:05 +0200

    Cycles (l4_1) 3

--- sandbox/src/main/java/ua/stqa/test/sandbox/sandbox/Primes.java
+++ sandbox/src/main/java/ua/stqa/test/sandbox/sandbox/Primes.java
@@ -14,9 +14,18 @@ public class Primes {
 
   public static boolean isPrimeWhile(int n) {
     int i = 2;
-    while (i<n && n % i != 0) { 
+    while (i<n && n % i != 0) {
       i++;
     }
     return i == n;
+  }
+
+  public static boolean isPrime(long n) {
+    for (long i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
   }
 }
--- sandbox/src/test/java/ua/stqa/test/sandbox/PrimeTests.java
+++ sandbox/src/test/java/ua/stqa/test/sandbox/PrimeTests.java
@@ -7,12 +7,19 @@ import ua.stqa.test.sandbox.sandbox.Primes;
 
 public class PrimeTests {
   @Test
-  public void testPrimes() {
+  public void testPrime() {
     Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
   }
 
   @Test
-  public void testNonPrimes() {
+  public void testNonPrime() {
     Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+
+
+  @Test (enabled = false)
+  public void testPrimeLong() { //действия с длинными числами, всегда длинее
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
   }
 }
