a1a065ea3c72b0970cc64979b7b1c4c0b2c59253
13Dima13 <sda.freeman@gmail.com>
2016-03-20 19:11:46 +0200

    List <GroupData> (получение списка групп)

--- addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupCreationTests.java
@@ -4,18 +4,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.stqa.test.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    List <GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test4"));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before +1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() +1);
 
   }
 
