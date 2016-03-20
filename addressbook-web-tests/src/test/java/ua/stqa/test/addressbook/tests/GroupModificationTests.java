69f337642b553e449d5b9ead7761277f76fa26cb
13Dima13 <sda.freeman@gmail.com>
2016-03-20 19:38:34 +0200

    added HashSet

--- addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.stqa.test.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -14,11 +15,17 @@ public class GroupModificationTests extends TestBase{
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() -1 );
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test4"));
     app.getGroupHelper().submitGroupModification();
+    GroupData group = new GroupData("test2", "test3", "test4");
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() -1 );
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
