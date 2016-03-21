4099785c9272d9310d0bce1a9bcb5cfb39d290b9
13Dima13 <sda.freeman@gmail.com>
2016-03-22 00:16:36 +0200

    Optimization code for Groups

--- addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ua.stqa.test.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ua.stqa.test.addressbook.model.GroupData;
 
@@ -10,20 +11,26 @@ import java.util.List;
 public class GroupDeletionTests extends TestBase {
 
 
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
+  }
+
+
   @Test
   public void testGroupDeletion() {
-    app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    if (!app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("test2", null, null));
-    }
-    app.getGroupHelper().selectGroup(before.size() - 1);
+    int index = before.size() - 1;
+    app.getGroupHelper().selectGroup(index);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size() - 1);
+    Assert.assertEquals(after.size(), index);
 
-    before.remove(before.size() - 1);
+    before.remove(index);
     Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
     after.sort(byId);
