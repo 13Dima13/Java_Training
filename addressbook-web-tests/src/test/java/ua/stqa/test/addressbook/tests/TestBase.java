b55ca83917faa32cc625889d3cf73129a40525c3
Dm1try <dst@scalepoint.com>
2016-04-09 20:40:37 +0300

    Fix for HW 16

--- addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/stqa/test/addressbook/tests/TestBase.java
@@ -7,17 +7,18 @@ import ua.stqa.test.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app
-          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    ApplicationManager app;
 
-  @BeforeSuite
-  public void setUp() throws Exception {
-    app.init();
-  }
 
-  @AfterSuite
-  public void tearDown() {
-    app.stop();
-  }
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+        app.init();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        app.stop();
+    }
 
 }
